fbb3e9ea960d7cf29beaba9cb8b0ed378eb19f2c
Sunan Tajwar <tajwarsunan@gmail.com>
2021-10-24 09:25:54 -0400

    Initial Commit

---
+++ Instagram_Codepath/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Instagram_Codepath
+//
+//  Created by Sunan Tajwar on 10/24/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Instagram_CodepathTests/Instagram_CodepathTests.swift
@@ -0,0 +1,33 @@
+//
+//  Instagram_CodepathTests.swift
+//  Instagram_CodepathTests
+//
+//  Created by Sunan Tajwar on 10/24/21.
+//
+
+import XCTest
+@testable import Instagram_Codepath
+
+class Instagram_CodepathTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
