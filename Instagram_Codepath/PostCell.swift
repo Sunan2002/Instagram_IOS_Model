8241e856f05e348975beab739a70ec48297362e1
Sunan Tajwar <tajwarsunan@gmail.com>
2021-10-24 13:47:11 -0400

    Finished Instagram Part I

---
+++ Instagram_Codepath/PostCell.swift
@@ -0,0 +1,31 @@
+//
+//  PostCell.swift
+//  Instagram_Codepath
+//
+//  Created by Sunan Tajwar on 10/24/21.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    @IBOutlet weak var photoView: UIImageView!
+    
+    @IBOutlet weak var usernameLabel: UILabel!
+    
+    @IBOutlet weak var captionLabel: UILabel!
+    
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
